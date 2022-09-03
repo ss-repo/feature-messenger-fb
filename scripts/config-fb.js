7ca1d479de12886f579b7a5dcecb3a5b666eb3f0
Sergio <sergio.desenvolvedor.web@gmail.com>
2022-09-03 10:23:26 -0300

    fb-messenger-tests automations

---
+++ scripts/config-fb.js
@@ -0,0 +1,5 @@
+//Your config
+var chatbox = document.getElementById('fb-customer-chat');
+chatbox.setAttribute("page_id", "110382801780841");
+chatbox.setAttribute("attribution", "biz_inbox");
+//
---
+++ scripts/sdk-fb.js
@@ -0,0 +1,16 @@
+//Your SDK Code
+ window.fbAsyncInit = function() {
+    FB.init({
+      xfbml            : true,
+      version          : 'v14.0'
+    });
+  };
+
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/pt_BR/sdk/xfbml.customerchat.js';
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+  //
